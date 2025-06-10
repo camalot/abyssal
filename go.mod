9754a48683b9bae220e9b9c8de6d8ee20d883ff1
Ryan Conrad <camalot@gmail.com>
2025-06-10 00:01:41 -0500

    work on jira notifier

--- go.mod
+++ go.mod
@@ -3,6 +3,21 @@ module github.com/camalot/abyssal
 go 1.24.3
 
 require (
+	github.com/ctreminiom/go-atlassian/v2 v2.4.0
+	github.com/google/go-github/v72 v72.0.0
+	github.com/hashicorp/go-version v1.7.0
+	github.com/joho/godotenv v1.5.1
+	github.com/mikefarah/yq/v4 v4.45.4
+	github.com/sirupsen/logrus v1.9.3
+	github.com/spf13/cobra v1.9.1
+	github.com/spf13/viper v1.20.1
+	github.com/t-tomalak/logrus-easy-formatter v0.0.0-20190827215021-c074f06c5816
+	gopkg.in/op/go-logging.v1 v1.0.0-20160211212156-b2cb9fa56473
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	dario.cat/mergo v1.0.2 // indirect
 	github.com/a8m/envsubst v1.4.3 // indirect
 	github.com/alecthomas/participle/v2 v2.1.4 // indirect
 	github.com/dimchansky/utfbom v1.1.1 // indirect
@@ -12,33 +27,26 @@ require (
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.17.1 // indirect
-	github.com/google/go-github/v72 v72.0.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/hashicorp/go-version v1.7.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jinzhu/copier v0.4.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/magiconair/properties v1.8.10 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mikefarah/yq/v4 v4.45.4 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.12.0 // indirect
 	github.com/spf13/cast v1.7.1 // indirect
-	github.com/spf13/cobra v1.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
-	github.com/spf13/viper v1.20.1 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
-	github.com/t-tomalak/logrus-easy-formatter v0.0.0-20190827215021-c074f06c5816 // indirect
+	github.com/tidwall/gjson v1.18.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/yuin/gopher-lua v1.1.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
-	gopkg.in/op/go-logging.v1 v1.0.0-20160211212156-b2cb9fa56473 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- libs/notifiers/JiraNotifier.go
+++ libs/notifiers/JiraNotifier.go
@@ -1,9 +1,16 @@
 package notifiers
 
 import (
+	"context"
+	"fmt"
+	"net/url"
+	"os"
+
 	"github.com/camalot/abyssal/config"
 	"github.com/camalot/abyssal/libs/providers"
 	"github.com/camalot/abyssal/libs/templates"
+	jira "github.com/ctreminiom/go-atlassian/v2/jira/v3"
+	jiramodels "github.com/ctreminiom/go-atlassian/v2/pkg/infra/models"
 )
 
 /*
@@ -12,12 +19,15 @@ import (
 	title: "[Abyssal] Bump {{ .Target.Name }} from v{{ .CurrentVersion }} to v{{ .ExpectedVersion }}"
 	body: |
 		Bump {{ .Target.Name }} from v{{ .CurrentVersion }} to v{{ .ExpectedVersion }}.
-	user: "{{ .EnvironmentVariables.JIRA_USER }}"
+	user: "{{ .EnvironmentVariables.JIRA_USERNAME }}"
 	token: "{{ .EnvironmentVariables.JIRA_TOKEN }}"
 	url: "{{ .EnvironmentVariables.JIRA_URL }}"
-	project: "{{ .EnvironmentVariables.JIRA_PROJECT }}"
+	project: "{{ .EnvironmentVariables.JIRA_PROJECT_KEY }}"
+	board: "{{ .EnvironmentVariables.JIRA_BOARD_ID }}"
 	issueType: "{{ .EnvironmentVariables.JIRA_ISSUE_TYPE }}"
+	jql: project = {{ .EnvironmentVariables.JIRA_PROJECT_KEY }} AND issuetype = {{ .EnvironmentVariables.JIRA_ISSUE_TYPE }} AND status != Done
 	labels: ["abyssal"]
+	payload: {}
 */
 
 type JiraNotifier struct {
@@ -25,21 +35,30 @@ type JiraNotifier struct {
 	Title   string `yaml:"title"`
 	Body    string `yaml:"body"`
 
-	AccessToken string   `yaml:"token,omitempty"`
-	IssueLabels []string `yaml:"issueLabels,omitempty"`
-
-	Url       string `yaml:"url,omitempty"`
-	Project   string `yaml:"project,omitempty"`
-	IssueType string `yaml:"issueType,omitempty"`
-
+	Url            string                     `yaml:"url,omitempty"`
+	BoardId        string                     `yaml:"board,omitempty"`
+	Project        string                     `yaml:"project,omitempty"`
+	IssueType      string                     `yaml:"issueType,omitempty"`
+	IssueLabels    []string                   `yaml:"issueLabels,omitempty"`
+	JQL            string                     `yaml:"jql,omitempty"` // JQL query to search for existing issues
 	Authentication JiraNotifierAuthentication `yaml:"authentication,omitempty"`
 
 	NotifierConfig config.NotifierElement `yaml:"-"`
+
+	Payload interface{} `yaml:"payload,omitempty"`
 }
 
 type JiraNotifierAuthentication struct {
 	User  string `yaml:"user,omitempty"`
 	Token string `yaml:"token,omitempty"`
+}
+
+type JiraNotificationPayload struct {
+	Title       string   `json:"title"`
+	Body        string   `json:"body"`
+	IssueLabels []string `json:"issueLabels,omitempty"`
+
+	Result *providers.ProviderCheckResult `json:"result,omitempty"` // This can be used to store the result of the check that triggered the notification
 }
 
 func NewJiraNotifier(notifierElement config.NotifierElement, config *config.AppConfiguration) *JiraNotifier {
@@ -79,6 +98,11 @@ func NewJiraNotifier(notifierElement config.NotifierElement, config *config.AppC
 		project = templates.EnvironmentVariableTemplate(val.(string))
 	}
 
+	boardId := ""
+	if val, ok := notifierElement.Extra["board"]; ok {
+		boardId = templates.EnvironmentVariableTemplate(val.(string))
+	}
+
 	issueType := ""
 	if val, ok := notifierElement.Extra["issueType"]; ok {
 		issueType = templates.EnvironmentVariableTemplate(val.(string))
@@ -87,26 +111,111 @@ func NewJiraNotifier(notifierElement config.NotifierElement, config *config.AppC
 		issueType = "Task" // Default issue type if not specified
 	}
 
+	jql := ""
+	if val, ok := notifierElement.Extra["jql"]; ok {
+		jql = templates.EnvironmentVariableTemplate(val.(string))
+	}
+
+	var payload interface{}
+	payload = nil
+	if val, ok := notifierElement.Extra["payload"]; ok {
+		payload = val
+	}
+
 	return &JiraNotifier{
 		Enabled:        notifierElement.Enabled,
 		Title:          title,
 		Body:           body,
-		AccessToken:    token,
 		IssueLabels:    labels,
 		NotifierConfig: notifierElement,
 
-		Url:            url,
-		Project:        project,
-		IssueType:      issueType,
+		Url:       url,
+		Project:   project,
+		BoardId:   boardId,
+		IssueType: issueType,
+		JQL:       jql,
+
 		Authentication: JiraNotifierAuthentication{
 			User:  templates.EnvironmentVariableTemplate(username),
 			Token: templates.EnvironmentVariableTemplate(token),
 		},
+		Payload: payload,
 	}
+}
+
+func arrayToJqlList(array []string) string {
+	if len(array) == 0 {
+		return ""
+	}
+	jqlList := ""
+	for i, item := range array {
+		if i > 0 && i < len(array)-1 {
+			jqlList += ", "
+		}
+		jqlList += fmt.Sprintf("\"%s\"", item)
+	}
+	return jqlList
+}
+
+func (j *JiraNotifier) findIssues(title string, states []string, labels []string) ([]*jiramodels.IssueScheme, error){
+	if !j.Enabled {
+		return nil, fmt.Errorf("findIssues called on disabled Jira notifier")
+	}
+	host, err := j.getHost()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error getting Jira host: %v\n", err)
+		return nil, err
+	}
+	client, err := jira.New(nil, host)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error creating Jira client: %v\n", err)
+		return nil, err
+	}
+
+	// if this doesn't work, might have to set to use SetBasicAuth instead
+	client.Auth.SetBearerToken(j.Authentication.Token)
+
+	// take the JQL query from the notifier config
+	// and append the title to it
+	jql := fmt.Sprintf(
+		" %s AND summary ~ \"%s\" AND status IN (%s) AND labels IN (%s)",
+		j.JQL,
+		title,
+		arrayToJqlList(states),
+		arrayToJqlList(labels),
+	)
+	fields := []string{"summary", "status", "assignee", "reporter", "created", "updated", "labels"}
+	expands := []string{"renderedFields", "changelog"}
+	issues, response, err := client.Issue.Search.SearchJQL(context.Background(), jql, fields, expands, 1, "")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error searching Jira issues: %v\n", err)
+		return nil, err
+	}
+	if response.StatusCode != 200 {
+		return nil, fmt.Errorf("error searching Jira issues: %s", response.Status)
+	}
+
+	return issues.Issues, nil
+}
+
+func (j *JiraNotifier) getHost() (string, error) {
+	if j.Url == "" {
+		return "", fmt.Errorf("jira notifier URL is not set. Please check your configuration")
+	}
+	// Parse the URL to extract the host
+	parsedUrl, err := url.Parse(j.Url)
+	if err != nil {
+		return "", err
+	}
+	return parsedUrl.Host, nil
 }
 
 // Notify sends a notification with the given payload.
 func (j *JiraNotifier) Notify(payload interface{}) error {
+	if !j.Enabled {
+		return nil
+	}
 	// Implementation for sending a notification to Jira
 	// This would typically involve using the Jira API to create an issue or comment
 	return nil
@@ -115,32 +224,77 @@ func (j *JiraNotifier) Notify(payload interface{}) error {
 // this method checks if the notifier needs to send a notification
 // this can be used to avoid duplicate notifications
 func (j *JiraNotifier) NeedsNotification(payload interface{}) bool {
+	if !j.Enabled {
+		return false
+	}
 	// Implementation for sending a notification to Jira
 	// This would typically involve using the Jira API to create an issue or comment
 	return false
 }
+
 func (j *JiraNotifier) HasNotification(payload interface{}) (bool, []interface{}) {
-	// Implementation for sending a notification to Jira
-	// This would typically involve using the Jira API to create an issue or comment
-	return false, nil
+	if !j.Enabled {
+		return false, nil
+	}
+	issues, err := j.findIssues(j.Title, []string{"Open", "In Progress"}, j.IssueLabels)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error finding Jira issues: %v\n", err)
+		return false, nil
+	}
+	if len(issues) == 0 {
+		return false, nil
+	}
+
+	hasIssues := len(issues) > 0
+	if !hasIssues {
+		return false, nil
+	}
+	issuesList := make([]interface{}, len(issues))
+	for i, issue := range issues {
+		issuesList[i] = issue
+	}
+	return true, issuesList
 }
 
 func (j *JiraNotifier) CloseNotification(payload interface{}) error {
+	if !j.Enabled {
+		return nil
+	}
 	// Implementation for sending a notification to Jira
 	// This would typically involve using the Jira API to create an issue or comment
 	return nil
 }
 
 func (j *JiraNotifier) CreatePayload(config config.NotifierElement, result *providers.ProviderCheckResult) (interface{}, error) {
-	// Implementation for sending a notification to Jira
-	// This would typically involve using the Jira API to create an issue or comment
-	return nil, nil
+	if !j.Enabled {
+		return nil, nil
+	}
+	template := templates.NewTemplate("jira-notification", j.Title, result)
+	renderedTitle, err := template.Render()
+	if err != nil {
+		return nil, err // Error occurred while rendering the title
+	}
+
+	template = templates.NewTemplate("jira-notification-body", j.Body, result)
+	renderedBody, err := template.Render()
+	if err != nil {
+		return nil, err // Error occurred while rendering the body
+	}
+	return JiraNotificationPayload{
+		Title:  renderedTitle,
+		Body:   renderedBody,
+		Result: result,
+	}, nil
 }
+
 func (j *JiraNotifier) GetNotifierConfig() config.NotifierElement {
 	return j.NotifierConfig
 }
 
 func (j *JiraNotifier) ProcessResult(result *providers.ProviderCheckResult) error {
+	if !j.Enabled {
+		return nil
+	}
 	// Implementation for sending a notification to Jira
 	// This would typically involve using the Jira API to create an issue or comment
 	return nil
