f4bd4a50291d7f46f709652bbb263bdbc8313fbb
Ryan Conrad <camalot@gmail.com>
2025-06-20 23:52:37 -0500

    implement version cache of target

--- cmd/check.go
+++ cmd/check.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/camalot/abyssal/libs/notifiers"
 	"github.com/camalot/abyssal/libs/providers"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -17,30 +22,65 @@ var CheckCmd = &cobra.Command{
 	Long:  `Check for outdated packages in your project.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
+		Logger.Out = os.Stderr
+		start := time.Now()
+
+		// prepare notifiers
+		notifiersList := make([]notifiers.Notifier, 0)
+		if len(Config.Settings.Notifiers) >= 0 {
+			for _, notifier := range Config.Settings.Notifiers {
+				n := notifiers.NewNotifier(notifier, Config)
+				if n == nil {
+					Logger.Warnf("Notifier type %s is not supported or not implemented", notifier.Type)
+					continue
+				}
+				if !n.IsEnabled() {
+					Logger.Debugf("Notifier %s is disabled, skipping", n.GetName())
+					continue
+				}
+				notifiersList = append(notifiersList, n)
+			}
+		}
+
 		for _, provider := range Config.Providers {
+
 			p := providers.NewProvider(provider, Config)
 			if err := p.Load(); err != nil {
 				Logger.Fatalf("Error loading Argo provider: %v", err)
 			}
+			fmt.Fprintf(os.Stdout, "Provider: %s\n", p.GetName())
+
 			targets, err := p.GetTargets()
 			if err != nil {
 				Logger.Fatalf("Error getting targets from provider: %v", err)
 			}
+			if len(targets) == 0 {
+				Logger.Warnf("No targets found for provider %s", provider.Type)
+				continue
+			}
+
 			for _, target := range targets {
+				fmt.Fprintf(os.Stdout, "\nTarget: %s\n", target.Name)
+				fmt.Fprintf(os.Stdout, "Source: %s\n", target.Source)
 				// convert the targets in to Packages
 				result, err := p.CheckVersionOutOfDate(target)
 				if err != nil {
-					Logger.Errorf("Error checking package %s: %v", target.Name, err)
+					fmt.Fprintf(os.Stderr, "Error checking package %s: %v\n", target.Name, err)
 					continue
 				}
-				if result.Outdated {
-					Logger.Warnf("%s is out of date! Current version: %s - Latest version: %s", target.Name, result.CurrentVersion, result.ExpectedVersion)
-				} else {
-					Logger.Infof("%s is up to date.", target.Name)
-				}
 
+				for _, notifier := range notifiersList {
+					err := notifier.ProcessResult(&result)
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "Error processing result with notifier %s: %v\n", notifier.GetName(), err)
+						continue
+					}
+				}
 			}
 		}
+
+		duration := time.Since(start)
+		fmt.Fprintf(os.Stdout, "Execution Duration: %s\n\n", duration)
 	},
 }
 
